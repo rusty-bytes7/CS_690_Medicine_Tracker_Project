a09f8f904747c11f1fb5ff588e4e62f79f1abad6
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-03-27 16:14:59 -0400

    made some preliminary functionality

--- MedicineTracker/Tracker/Program.cs
+++ MedicineTracker/Tracker/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace Tracker;
 
+using Spectre.Console;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello!");
+        Console.WriteLine("Please input your name: ");
+        string name = Console.ReadLine() ?? "User";
+        AnsiConsole.Write(new FigletText($"Welcome, {name}, to the Medicine Tracker!")
+        .Centered()
+        .Color(Color.Purple_1));
+
+        AnsiConsole.WriteLine("This is a medicine tracker that allows you to track your medicines and their dosages.");
+        AnsiConsole.WriteLine("You can add, remove, and view your medicines and their dosages.");
+        AnsiConsole.WriteLine("You can also set reminders for your medicines and their dosages, and view your information in a table.");
+        AnsiConsole.WriteLine("\n");
+
+        AnsiConsole.WriteLine("What would you like to do?");
+        AnsiConsole.WriteLine("1. Add a medicine and dosage\n2. Remove or edit a medicine and dosage\n3. View medicines and dosages\n4. View reminders\n5. View information in a table\n6. Exit");
+
     }
 }
