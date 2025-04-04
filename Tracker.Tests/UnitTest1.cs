721ee85c67edcf879214482cc1c874c0b84a7015
rusty-bytes7 <147008821+rusty-bytes7@users.noreply.github.com>
2025-04-03 20:19:50 -0400

    set up testing

---
+++ Tracker.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tracker.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        //basic test just to get tests going
+        Assert.Equal(1, 1);
+    }
+}
